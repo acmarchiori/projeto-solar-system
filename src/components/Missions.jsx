bb0551495ae2e8ba6e00b7a457de10985f789b60
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-24 17:46:45 -0300

    Finalizado requisito 7

---
+++ src/components/Missions.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Missions extends React.Component {
+  render() {
+    return (
+      <header>
+        <div data-testid="missions" />
+      </header>
+    );
+  }
+}
+
+export default Missions;
