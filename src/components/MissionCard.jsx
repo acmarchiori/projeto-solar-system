608153bc3bc01c14ed185ac1e408994f129cf30a
acmarchiori <ac_marchiori@yahoo.com.br>
2023-01-24 18:05:46 -0300

    Finalizado requisito 9

---
+++ src/components/MissionCard.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+class MissionCard extends React.Component {
+  render() {
+    const { name, year, country, destination } = this.props;
+    return (
+      <header>
+        <div data-testid="mission-card">
+          <p data-testid="mission-name">{ name }</p>
+          <p data-testid="mission-year">{ year }</p>
+          <p data-testid="mission-country">{ country }</p>
+          <p data-testid="mission-destination">{ destination }</p>
+        </div>
+      </header>
+    );
+  }
+}
+
+MissionCard.propTypes = {
+  name: PropTypes.string,
+  year: PropTypes.string,
+  country: PropTypes.string,
+  destination: PropTypes.string,
+}.isRequired;
+
+export default MissionCard;
